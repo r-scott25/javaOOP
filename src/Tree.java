2bece08eaf6b2caf8da62a24a1e1994d018a7c51
r-scott25 <rachelbernal2@gmail.com>
2022-12-07 18:27:12 -0600

    Initial commit

---
+++ src/Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+    double heightFt;
+    double trunkDiameterInches;
+    TreeType treeType;
+
+    void grow() {
+        this.heightFt = this.heightFt + 10;
+        this.trunkDiameterInches = this.trunkDiameterInches + 1;
+    }
+
+}
